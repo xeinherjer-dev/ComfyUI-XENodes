c4d3f35274a048b643d0906c1ae5ce1f111fe039
xeinherjer <112741359+xeinherjer-dev@users.noreply.github.com>
2026-04-20 02:53:11 +0900

    fix: increase minimum widget height to 150px in save_video.js

--- web/js/save_video.js
+++ web/js/save_video.js
@@ -145,7 +145,7 @@ app.registerExtension({
 							Object.defineProperty(widget, 'computeLayoutSize', {
 								configurable: true,
 								get() {
-									return () => ({ minWidth: 50, minHeight: 50 });
+									return () => ({ minWidth: 50, minHeight: 150 });
 								},
 								set(_fn) {
 									// Intentionally ignore attempts to override (e.g. from useNodeVideo)
