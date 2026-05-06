d2cb4e3082099e787f0d012cc13999054914a284
xeinherjer <112741359+xeinherjer-dev@users.noreply.github.com>
2026-05-06 09:30:57 +0900

    feat: add frontend extension for XENodes.SaveHDRImage to handle dynamic CRF updates and filename prefix replacements

---
+++ web/js/save_hdr_image.js
@@ -0,0 +1,50 @@
+import { app } from "../../../scripts/app.js";
+import { applyTextReplacements } from "../../../scripts/utils.js";
+
+app.registerExtension({
+	name: "XENodes.SaveHDRImage",
+	async beforeRegisterNodeDef(nodeType, nodeData, app) {
+		if (nodeData.name === "XENodes.SaveHDRImage") {
+			const onNodeCreated = nodeType.prototype.onNodeCreated;
+			nodeType.prototype.onNodeCreated = function () {
+				const r = onNodeCreated ? onNodeCreated.apply(this, arguments) : undefined;
+
+				const codecWidget = this.widgets.find((w) => w.name === "codec");
+				const crfWidget = this.widgets.find((w) => w.name === "crf");
+				const prefixWidget = this.widgets.find((w) => w.name === "filename_prefix");
+
+				if (prefixWidget) {
+					prefixWidget.serializeValue = () => {
+						return applyTextReplacements(app, prefixWidget.value);
+					};
+				}
+
+				const CODEC_CRF_MAP = {
+					'av1': 10,
+					'av1_nvenc': 10 // Defaulting to 10 as per python file, but can be adjusted
+				};
+
+				const updateCrf = () => {
+					const codec = codecWidget?.value;
+					const crfValue = CODEC_CRF_MAP[codec];
+					if (crfValue !== undefined && crfWidget && (crfWidget.value === 0 || crfWidget.value === crfWidget.options.default)) {
+						crfWidget.value = crfValue;
+					}
+				};
+
+				if (codecWidget && crfWidget) {
+					const origCodecCallback = codecWidget.callback;
+					codecWidget.callback = function (v) {
+						updateCrf();
+						return origCodecCallback ? origCodecCallback.apply(this, arguments) : undefined;
+					};
+
+					// Initial sync
+					updateCrf();
+				}
+
+				return r;
+			};
+		}
+	},
+});
